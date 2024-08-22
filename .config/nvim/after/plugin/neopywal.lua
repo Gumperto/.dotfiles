e9db1e449cd929e7f6e4ce33443c96ecf7395b82
gumperto <162938203+Gumperto@users.noreply.github.com>
2024-08-22 11:37:21 +0700

    Added emergency fallback colorscheme

--- .config/nvim/after/plugin/neopywal.lua
+++ .config/nvim/after/plugin/neopywal.lua
@@ -1,3 +1,11 @@
 local neopywal = require("neopywal")
-neopywal.setup()
+
+neopywal.setup({})
+
+-- Emergency colorscheme in case 
+-- colorscheme is bad for text editing (ert for emergency retheme; rt for retheme)
+
+vim.api.nvim_set_keymap('n', '<leader>ert', ':colorscheme tokyonight-moon<CR>', { noremap = true, silent = false })
+vim.api.nvim_set_keymap('n', '<leader>rt', ':colorscheme neopywal<CR>', { noremap = true, silent = false })
+
 vim.cmd.colorscheme("neopywal")
