e5cf9cfac2159ae47b1aa005563cdbd81abe4ac9
gumperto <162938203+Gumperto@users.noreply.github.com>
2024-10-20 11:54:39 +0900

    Modified Hyprland dots for gamemode

--- .config/hypr/scripts/random_wallpaper.sh
+++ .config/hypr/scripts/random_wallpaper.sh
@@ -3,6 +3,6 @@
 directory=$HOME/wallpaper
 
 if [ -d "$directory" ]; then
-    random_background=$(ls $directory/wallpaper-* | shuf -n 1)
+    random_background=$(ls $directory/* | shuf -n 1)
     cp "$random_background" $HOME/current-wallpaper/current-wallpaper
 fi
